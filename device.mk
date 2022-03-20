da26e92fb2abad5f3f89590011457a4f642087e6
Thomas Makin <halorocker89@gmail.com>
2022-03-20 04:38:34 +0000

    nx: sensors: init: Add sensor calibration script
    - Add sensor calibration init service and script
    - Update sensor defconfig to support "factory" calibration
    
    Change-Id: I7b9660d9bfe230c7aef90a381f97ed3c23d03361

--- device.mk
+++ device.mk
@@ -135,7 +135,8 @@ endif
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     android.hardware.sensors@1.0-impl \
-    sensors.stmicro
+    sensors.stmicro \
+    sensors_calib
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \

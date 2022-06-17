77ba420be3cbbed072467f6830e59754ab42443d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-18 01:50:28 +0200

    nx: Cleanup fstab/initfiles situation
    * We automatically get /dev/block/by-name symlinks from init thanks to androidboot.boot_devices cmdline.
    * Add /vendor entry and mark it as recoveryonly since it's mounted from kernel.
    * [npjohnson]: nx_emmc/nx_sd are now merged into single build, u-boot selects this and abstracts it
      to the user.
    
    Change-Id: Iff8876e109234f418c58ab7b14036776c7293526

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -30,22 +30,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE               := init.nx_common.rc
-LOCAL_MODULE_CLASS         := ETC
-LOCAL_SRC_FILES            := init.nx_common.rc
-LOCAL_VENDOR_MODULE        := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE               := init.nx_emmc.rc
-LOCAL_MODULE_CLASS         := ETC
-LOCAL_SRC_FILES            := init.nx_emmc.rc
-LOCAL_VENDOR_MODULE        := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.nx.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.nx.rc
@@ -53,21 +37,7 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.nx_emmc.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.recovery.nx.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := power.nx.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_ODM_MODULE   := true
-LOCAL_SRC_FILES    := power.nx.rc
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := power.nx_emmc.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_ODM_MODULE   := true
 LOCAL_SRC_FILES    := power.nx.rc

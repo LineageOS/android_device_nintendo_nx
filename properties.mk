6495935e5a25e7448d3526b962c3b7eeb919f06f
Thomas Makin <halorocker89@gmail.com>
2022-07-09 23:54:56 -0400

    nx: Set HWC properties
    Change-Id: I56c895563a669ea5eca0726ae3722af8a1ca4098

--- properties.mk
+++ properties.mk
@@ -10,6 +10,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     persist.sys.NV_ECO.IF.CHARGING=false
 
+# HWC
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.tegra.hdmi.autorotate=0 \
+    persist.vendor.tegra.stb.mode=1
+
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.sys.usb.udc=700d0000.xudc \

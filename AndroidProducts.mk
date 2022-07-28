83bfec557f151bd2cc9d47e7c2492832d9b03371
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-28 18:23:12 -0400

    nx: Intial rebrand from foster
    * This is a tree for all Nintendo Switch variants:
      `odin`, `modin`, `vali`, and `frig`.
    * `ro.hardware` will be NX on all variants with
       `ro.hardware.sku` being set per variant by u-boot.
    
    Changes are as follows:
    * At the moment the packaged DTB is only for `odin`, more to follow.
     * Note: We may make `modin`, `vail`, and `frig` device-tree-overlays
       on the `odin` base DTB to simplify things.
    * Use darcy's thermal config as it's the most lenient, and is a same SoC STB.
    * Update board-info to reflect latest coreboot bootloader.
    * Unlike `icosa_sr` previously did, we will use the BCM HAL for BT over
      BTLinux, as A2DP is tested and verified, no need to deviate.
    
    Changes that will require follow-up commits to build out:
    * Update init to reflect nx's needs - will need more work to support devices
      other than `odin`.
    
    Removals are as follows:
    * Remove all non-nx device support files.
    
    Co-authored-by: Thomas Makin <halorocker89@gmail.com>
    Co-authored-by: Billy Laws <blaws05@gmail.com>
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: CTCaer <ctcaer@gmail.com>
    Co-authored-by: John Smith <nemui3353@gmail.com>
    Co-authored-by: langerhans <langerhans@users.noreply.github.com>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I9bab4d94c6e94a9c6e2a7cf04d16c4b76c0b6ac5

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_foster.mk \
-    $(LOCAL_DIR)/lineage_foster.mk \
-    $(LOCAL_DIR)/twrp_foster.mk
+    $(LOCAL_DIR)/lineage_nx.mk \
+    $(LOCAL_DIR)/twrp_nx.mk

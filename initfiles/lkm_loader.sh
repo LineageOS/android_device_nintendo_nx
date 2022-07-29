20f478db3336552c981f3af192ff2fc24daa8b32
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-28 20:47:07 -0400

    nx: lkm_loader: Remove irrelevant modules/guards
    * We don't care about other nv devices.
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Co-authored-by: Thomas Makin <halorocker89@gmail.com>
    Co-authored-by: Billy Laws <blaws05@gmail.com>
    Change-Id: Ib2f7711c87df2b25d8497eebdb9e71975566a3ab

--- initfiles/lkm_loader.sh
+++ initfiles/lkm_loader.sh
@@ -52,26 +52,7 @@ if [ "`cat /proc/device-tree/brcmfmac_pcie_wlan/status`" = "okay" ]; then
         do_insmod /vendor/lib/modules/cy_cfg80211.ko
         do_insmod /vendor/lib/modules/brcmutil.ko
         do_insmod /vendor/lib/modules/brcmfmac.ko
-elif [ "`cat /proc/device-tree/bcmdhd_pcie_wlan/status`" = "okay" ]; then
-        /vendor/bin/log -t "wifiloader" -p i " Loading bcmdhd_pcie driver for wlan"
-        do_insmod /vendor/lib/modules/cfg80211.ko
-        do_insmod /vendor/lib/modules/bcmdhd_pcie.ko
-elif [ "`cat /proc/device-tree/bcmdhd_wlan/status`" = "okay" ]; then
-        /vendor/bin/log -t "wifiloader" -p i " Loading bcmdhd driver for wlan"
-        do_insmod /vendor/lib/modules/cfg80211.ko
-        do_insmod /vendor/lib/modules/bcmdhd.ko
 fi
-if [[ "$hardwareName" = +(porg*) ]]; then
-	do_insmod /vendor/lib/modules/btbcm.ko
-	do_insmod /vendor/lib/modules/btintel.ko
-	do_insmod /vendor/lib/modules/btrtl.ko
-	do_insmod /vendor/lib/modules/btusb.ko
-fi
-if [[ "$hardwareName" = "batuu" ]]; then
-        do_insmod /vendor/lib/modules/cfg80211.ko
-	do_insmod /vendor/lib/modules/rtl8821cu.ko
-fi
-
 # USB-to-serial driver
 if [ "$hardwareName" != "sif" ]; then
     do_insmod /vendor/lib/modules/usbserial.ko
@@ -148,9 +129,6 @@ do_insmod /vendor/lib/modules/trusty-ote.ko
 do_insmod /vendor/lib/modules/trusty-otf-iface.ko
 do_insmod /vendor/lib/modules/trusty-mem.ko
 do_insmod /vendor/lib/modules/trusty-virtio.ko
-if [ "$hardwareName" = "foster_e_hdd" ]; then
-    do_insmod /vendor/lib/modules/tegra_cpc.ko
-fi
 
 # Peripheral
 do_insmod /vendor/lib/modules/core.ko
@@ -209,22 +187,6 @@ fi
 do_insmod /vendor/lib/modules/ina230.ko
 do_insmod /vendor/lib/modules/ina3221.ko
 
-# Camera sensors
-/vendor/bin/log -t "$scriptName" -p i "loading ODM Camera started"
-if [[ "$hardwareName" != +(*foster*|*darcy*|sif) ]]; then
-    do_insmod /vendor/lib/modules/pca9570.ko
-    do_insmod /vendor/lib/modules/tc358840.ko
-    do_insmod /vendor/lib/modules/ov5693.ko
-    do_insmod /vendor/lib/modules/ov9281.ko
-    do_insmod /vendor/lib/modules/ov10823.ko
-    do_insmod /vendor/lib/modules/ov23850.ko
-    do_insmod /vendor/lib/modules/lc898212.ko
-    do_insmod /vendor/lib/modules/imx185.ko
-    do_insmod /vendor/lib/modules/imx219.ko
-    do_insmod /vendor/lib/modules/imx274.ko
-fi
-/vendor/bin/log -t "$scriptName" -p i "loading ODM Camera completed"
-
 /vendor/bin/log -t "$scriptName" -p i "Loading vendor gpio timed keys module for early mode started"
 do_insmod /vendor/lib/modules/gpio_timed_keys.ko
 /vendor/bin/log -t "$scriptName" -p i "Loading vendor gpio timed keys module for early mode completed"
@@ -236,13 +198,8 @@ if [ "$hardwareName" != "sif" ]; then
     do_insmod /vendor/lib/modules/si2157.ko
     do_insmod /vendor/lib/modules/lgdt3305.ko
     do_insmod /vendor/lib/modules/tda18272.ko
-    do_insmod /vendor/lib/modules/em28xx.ko
-    do_insmod /vendor/lib/modules/em28xx-dvb.ko
-    do_insmod /vendor/lib/modules/em28xx-rc.ko
     do_insmod /vendor/lib/modules/cx25840.ko
     do_insmod /vendor/lib/modules/cx2341x.ko
-    do_insmod /vendor/lib/modules/cx231xx.ko
-    do_insmod /vendor/lib/modules/cx231xx-dvb.ko
     /vendor/bin/log -t "$scriptName" -p i "Early loading ODM TV tuner modules completed"
 fi
 
@@ -281,21 +238,6 @@ do_insmod /vendor/lib/modules/hid-betopff.ko
 # insmod /odm/lib/modules/example2.ko
 # Upstream has these on odm, but we put all modules on vendor
 /vendor/bin/log -t "$scriptName" -p i "Loading LKM Board-ODM modules started"
-
-# Load Sensor modules
-/vendor/bin/log -t "$scriptName" -p i "loading ODM sensor started"
-if [[ "$hardwareName" != +(*foster*|*darcy*|sif) ]]; then
-    do_insmod /vendor/lib/modules/nvs.ko
-    do_insmod /vendor/lib/modules/nvi-mpu.ko
-    do_insmod /vendor/lib/modules/nvi-ak89xx.ko
-    do_insmod /vendor/lib/modules/nvi-bmpX80.ko
-    do_insmod /vendor/lib/modules/nvs_a3g4250d.ko
-    do_insmod /vendor/lib/modules/nvs_ais328dq.ko
-    do_insmod /vendor/lib/modules/nvs_bh1730fvc.ko
-    do_insmod /vendor/lib/modules/nvs_cm3218.ko
-    do_insmod /vendor/lib/modules/nvs_dfsh.ko
-fi
-/vendor/bin/log -t "$scriptName" -p i "loading ODM sensor completed"
 
 do_insmod /vendor/lib/modules/gps_wake.ko
 /vendor/bin/log -t "$scriptName" -p i "loading comms modules completed"

803da5646ef950ecbcdb9a55cca09401a3546c51
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-30 00:53:17 -0400

    nx: Only build DTB files relevant to `nx` devices
    * Remove DTB custom mkbootimg in the process, as it is
      only for mdarcy/sif DTB's.
    
    Change-Id: I3aa19c267d290bb24a0ec05d2f4add03a820a70a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,11 +30,6 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE      := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := nx,nx_tab
 
-# Boot image
-BOARD_CUSTOM_BOOTIMG    := true
-BOARD_CUSTOM_BOOTIMG_MK := device/nvidia/foster/mkbootimg.mk
-BOARD_MKBOOTIMG_ARGS    := --header_version 1
-
 # Bootloader versions
 TARGET_BOARD_INFO_FILE := device/nintendo/nx/board-info.txt
 

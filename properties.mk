28ea65e2783c74e8c9e0a9a8ac561e6e982e291e
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-30 02:00:50 -0400

    nx: Remove irrelevant power brick property
    * Only used on Shield devices.
    
    Change-Id: Ic91eb785d515ea19f8cc574a26506c25ff9744bc

--- properties.mk
+++ properties.mk
@@ -10,10 +10,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     persist.sys.NV_ECO.IF.CHARGING=false
 
-# Power brick info
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.power.brick=PB1000
-
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.sys.usb.udc=700d0000.xudc \

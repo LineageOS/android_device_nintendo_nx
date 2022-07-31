1adef049d8b6c1d23e694ee1cc215643cd37086a
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-31 11:43:56 -0400

    nx: Add support for tablet style builds
    * `nx_tab` build target.
    * Split TV/Tablet build characteristics and DPI.
    
    Change-Id: I1c94647bb2e43fdc04a6f33249585a24a2e310bc

--- Android.mk
+++ Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter $(TARGET_DEVICE), nx),)
+ifneq ($(filter $(TARGET_DEVICE), nx nx_tab),)
 
 LOCAL_PATH := $(call my-dir)
 

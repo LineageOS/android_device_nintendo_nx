2e2c25eae4aa8e3b5e3c87ff1992d878ba8791d4
Nolen Johnson <johnsonnolen@gmail.com>
2022-07-31 11:43:56 -0400

    nx: Add support for tablet style builds
    * `nx_tab` build target.
    * Guard tablet specific permissions.
    * Guard TV specific DPI
    
    Change-Id: I1c94647bb2e43fdc04a6f33249585a24a2e310bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_COPY_OUT_VENDOR             := vendor
 BOARD_BUILD_SYSTEM_ROOT_IMAGE      := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := nx
+TARGET_OTA_ASSERT_DEVICE := nx,nx_tab
 
 # Boot image
 BOARD_CUSTOM_BOOTIMG    := true

b93c2f2300baf07918a559d040433cff4827a5a7
Thomas Makin <halorocker89@gmail.com>
2022-08-14 22:49:12 -0400

    nx: cec: Use the AOSP CEC HAL
    * `nx` does not support nvhdmi.
    
    Change-Id: Ie68babe3240b9f50cc99f80819cbbe66bdc94199

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ TARGET_TEGRA_VARIANT    ?= common
 
 TARGET_TEGRA_AUDIO    ?= nvaudio
 TARGET_TEGRA_BT       ?= bcm
-TARGET_TEGRA_CEC      ?= nvhdmi
+TARGET_TEGRA_CEC      ?= aosp
 TARGET_TEGRA_KERNEL   ?= 4.9
 TARGET_TEGRA_KEYSTORE ?= nvkeystore
 TARGET_TEGRA_MEMTRACK ?= nvmemtrack

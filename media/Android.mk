523d8db696ae27649214ef7def5ec09373cf934d
Nolen Johnson <johnsonnolen@gmail.com>
2022-11-21 19:11:03 -0500

    nx: Intial rebrand from foster
    * This is a tree for all Nintendo Switch variants:
      `odin`, `modin`, `vali`, and `frig`.
    * `ro.hardware` will be NX on all variants with
       `ro.hardware.sku` being set per variant by u-boot.
    
    Changes are as follows:
    * At the moment the packaged DTB is only for `odin`, more to follow.
     * Note: We may make `modin`, `vail`, and `frig` device-tree-overlays
       on the `odin` base DTB to simplify things.
    * Use darcy's thermal config as it's the most lenient, and is a same SoC STB.
    * Update board-info to reflect latest coreboot bootloader.
    * Unlike `icosa_sr` previously did, we will use the BCM HAL for BT over
      BTLinux, as A2DP is tested and verified, no need to deviate.
    
    Changes that will require follow-up commits to build out:
    * Update init to reflect nx's needs - will need more work to support devices
      other than `odin`.
    
    Removals are as follows:
    * Remove all non-nx device support files.
    
    Co-authored-by: Thomas Makin <halorocker89@gmail.com>
    Co-authored-by: Billy Laws <blaws05@gmail.com>
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    Co-authored-by: CTCaer <ctcaer@gmail.com>
    Co-authored-by: John Smith <nemui3353@gmail.com>
    Co-authored-by: langerhans <langerhans@users.noreply.github.com>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I9bab4d94c6e94a9c6e2a7cf04d16c4b76c0b6ac5

--- media/Android.mk
+++ media/Android.mk
@@ -27,66 +27,10 @@ LOCAL_MODULE        := audio_policy_configuration.xml
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := ETC
 ifeq ($(PRODUCT_IS_ATV),true)
-ifeq ($(TARGET_TEGRA_DOLBY),true)
-LOCAL_SRC_FILES     := audio_policy_configuration_nv_tv.xml
-else
 LOCAL_SRC_FILES     := audio_policy_configuration_nv_tv_nodolby.xml
-endif
-else
-ifeq ($(TARGET_TEGRA_DOLBY),true)
-LOCAL_SRC_FILES     := audio_policy_configuration_nv.xml
 else
 LOCAL_SRC_FILES     := audio_policy_configuration_nv_nodolby.xml
 endif
-endif
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := dragon_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := dragon_nvaudio_conf.xml
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := icosa_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := icosa_nvaudio_conf.xml
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := icosa_emmc_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := icosa_nvaudio_conf.xml
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := loki_e_base_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := loki_nvaudio_conf.xml
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := loki_e_lte_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := loki_nvaudio_conf.xml
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE        := loki_e_wifi_nvaudio_conf.xml
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := ETC
-LOCAL_SRC_FILES     := loki_nvaudio_conf.xml
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 

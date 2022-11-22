f9e8659ee19f42075c81f3c39a57e5c670c55357
Nolen Johnson <johnsonnolen@gmail.com>
2022-11-22 01:47:27 -0500

    nx: releasetools: Remove fuse status detection
    * All nx's are fused.
    
    Change-Id: Ifdbaebf80eeade7f6494483f90cffff673ae492e

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -19,22 +19,12 @@ import common
 import re
 import os
 
-APP_PART     = '/dev/block/by-name/APP'
-DTB_PART     = '/dev/block/by-name/DTB'
-STAGING_PART = '/dev/block/by-name/USP'
-VBMETA_PART  = '/dev/block/by-name/vbmeta'
-VENDOR_PART  = '/dev/block/by-name/vendor'
-NX_FILES     = '/mnt/vendor/hos_data'
+APP_PART      = '/dev/block/by-name/APP'
+DTB_PART      = '/dev/block/by-name/DTB'
+VENDOR_PART   = '/dev/block/by-name/vendor'
+NX_FILES      = '/mnt/vendor/hos_data'
 
-PUBLIC_KEY_PATH     = '/sys/devices/7000f800.efuse/7000f800.efuse:efuse-burn/public_key'
-FUSED_PATH          = '/sys/devices/7000f800.efuse/7000f800.efuse:efuse-burn/odm_production_mode'
-DTSFILENAME_PATH    = '/proc/device-tree/nvidia,dtsfilename'
-
-MODE_UNFUSED        = '0x00000000\n'
-MODE_FUSED          = '0x00000001\n'
-
-NX_PUBLIC_KEY       = '0x7e39e100d1135918ceedfe5d66e66496eed21ecb3486d72095cc0b7c60b8bd4f\n'
-NX_BL_VERSION       = '2020.04-03753-g53ff5d2195-rev6'
+NX_BL_VERSION = '2020.04-03753-g53ff5d2195-rev6'
 
 def FullOTA_PostValidate(info):
   if 'INSTALL/bin/resize2fs_static' in info.input_zip.namelist():
@@ -68,67 +58,29 @@ def AddBootloaderAssertion(info, input_zip):
     info.metadata["pre-bootloader"] = m.group(1)
 
 def AddBootloaderFlash(info, input_zip):
-  """ If device is fused """
-  info.script.AppendExtra('ifelse(')
-  info.script.AppendExtra('  read_file("' + FUSED_PATH + '") == "' + MODE_FUSED + '",')
-  info.script.AppendExtra('  (')
+  """ nx """
+  info.script.AppendExtra('  ifelse(')
+  info.script.AppendExtra('    getprop("ro.hardware") == "nx",')
+  info.script.AppendExtra('    (')
+  info.script.AppendExtra('      ifelse(')
+  info.script.AppendExtra('        getprop("ro.bootloader") == "' + NX_BL_VERSION + '",')
+  info.script.AppendExtra('        (')
+  info.script.AppendExtra('          ui_print("Correct bootloader already installed for " + getprop(ro.hardware));')
+  info.script.AppendExtra('        ),')
+  info.script.AppendExtra('        (')
+  info.script.AppendExtra('          ui_print("Flashing updated bootloader for " + getprop(ro.hardware));')
+  info.script.AppendExtra('          run_program("/system/bin/mkdir", "-p", "' + NX_FILES + '");')
+  info.script.AppendExtra('          run_program("/system/bin/mount", "/dev/block/by-name/hos_data", "' + NX_FILES + '");')
+  info.script.AppendExtra('          package_extract_file("firmware-update/coreboot.rom", "' + NX_FILES + '/switchroot/android/coreboot.rom");')
+  info.script.AppendExtra('          package_extract_file("firmware-update/boot.scr", "' + NX_FILES + '/switchroot/android/boot.scr");')
+  info.script.AppendExtra('          package_extract_file("firmware-update/bootlogo_android.bmp", "' + NX_FILES + '/switchroot/android/bootlogo_android.bmp");')
+  info.script.AppendExtra('          package_extract_file("firmware-update/icon_android_hue.bmp", "' + NX_FILES + '/switchroot/android/icon_android_hue.bmp");')
+  info.script.AppendExtra('          package_extract_file("firmware-update/00-android.ini", "' + NX_FILES + '/bootloader/ini/00-android.ini");')
+  info.script.AppendExtra('          run_program("/system/bin/umount", "' + NX_FILES + '");')
+  info.script.AppendExtra('        )')
+  info.script.AppendExtra('      );')
+  info.script.AppendExtra('      package_extract_file("install/" + tegra_get_dtbname(), "' + DTB_PART + '");')
+  info.script.AppendExtra('    )')
+  info.script.AppendExtra('  );')
 
-  """ Fused nx """
-  info.script.AppendExtra('    ifelse(')
-  info.script.AppendExtra('      getprop("ro.hardware") == "nx",')
-  info.script.AppendExtra('      (')
-  info.script.AppendExtra('        ifelse(')
-  info.script.AppendExtra('          getprop("ro.bootloader") == "' + NX_BL_VERSION + '",')
-  info.script.AppendExtra('          (')
-  info.script.AppendExtra('            ui_print("Correct bootloader already installed for fused " + getprop(ro.hardware));')
-  info.script.AppendExtra('          ),')
-  info.script.AppendExtra('          ifelse(')
-  info.script.AppendExtra('            read_file("' + PUBLIC_KEY_PATH + '") == "' + NX_PUBLIC_KEY + '",')
-  info.script.AppendExtra('            (')
-  info.script.AppendExtra('              ui_print("Flashing updated bootloader for fused " + getprop(ro.hardware));')
-  info.script.AppendExtra('              run_program("/system/bin/mkdir", "-p", "' + NX_FILES + '");')
-  info.script.AppendExtra('              run_program("/system/bin/mount", "/dev/block/by-name/hos_data", "' + NX_FILES + '");')
-  info.script.AppendExtra('              package_extract_file("firmware-update/coreboot.rom", "' + NX_FILES + '/switchroot/android/coreboot.rom");')
-  info.script.AppendExtra('              package_extract_file("firmware-update/boot.scr", "' + NX_FILES + '/switchroot/android/boot.scr");')
-  info.script.AppendExtra('              package_extract_file("firmware-update/bootlogo_android.bmp", "' + NX_FILES + '/switchroot/android/bootlogo_android.bmp");')
-  info.script.AppendExtra('              package_extract_file("firmware-update/icon_android_hue.bmp", "' + NX_FILES + '/switchroot/android/icon_android_hue.bmp");')
-  info.script.AppendExtra('              package_extract_file("firmware-update/00-android.ini", "' + NX_FILES + '/bootloader/ini/00-android.ini");')
-  info.script.AppendExtra('              run_program("/system/bin/umount", "' + NX_FILES + '");')
-  info.script.AppendExtra('            ),')
-  info.script.AppendExtra('            (')
-  info.script.AppendExtra('              ui_print("Unknown public key " + read_file("' + PUBLIC_KEY_PATH + '") + " for nx detected.");')
-  info.script.AppendExtra('              ui_print("This is not supported. Please report to LineageOS Maintainer.");')
-  info.script.AppendExtra('              abort();')
-  info.script.AppendExtra('            )')
-  info.script.AppendExtra('          )')
-  info.script.AppendExtra('        );')
-  info.script.AppendExtra('        package_extract_file("install/" + tegra_get_dtbname(), "' + DTB_PART + '");')
-  info.script.AppendExtra('      )')
-  info.script.AppendExtra('    );')
-
-  info.script.AppendExtra('  ),')
-
-  """ If not fused """
-  info.script.AppendExtra('  (')
-
-  """ Unfused nx """
-  info.script.AppendExtra('    ifelse(')
-  info.script.AppendExtra('      getprop("ro.hardware") == "nx",')
-  info.script.AppendExtra('      (')
-  info.script.AppendExtra('        ifelse(')
-  info.script.AppendExtra('          getprop("ro.bootloader") == "' + NX_BL_VERSION + '",')
-  info.script.AppendExtra('          (')
-  info.script.AppendExtra('            ui_print("Correct bootloader already installed for unfused " + getprop(ro.hardware));')
-  info.script.AppendExtra('          ),')
-  info.script.AppendExtra('          (')
-  info.script.AppendExtra('            ui_print("This is an unfused nx. Many devlopers would kill for this unit.");')
-  info.script.AppendExtra('            ui_print("This is not supported. Please report to LineageOS Maintainer.");')
-  info.script.AppendExtra('            abort();')
-  info.script.AppendExtra('          )')
-  info.script.AppendExtra('        );')
-  info.script.AppendExtra('        package_extract_file("install/" + tegra_get_dtbname(), "' + DTB_PART + '");')
-  info.script.AppendExtra('      )')
-  info.script.AppendExtra('    );')
-
-  info.script.AppendExtra('  )')
-  info.script.AppendExtra(');')
+  info.script.AppendExtra('),')

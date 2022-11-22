68974f87a5abbd42da390fbc413bd9b045a7036e
Nolen Johnson <johnsonnolen@gmail.com>
2022-11-22 15:21:37 -0500

    nx: Statically set USB ConfigFS properties
    * These won't vary per model.
    * Also alphabetize.
    
    Change-Id: Ie4f57c469ff49fcde35fc63b3f91180320ec2340

--- properties.mk
+++ properties.mk
@@ -12,5 +12,22 @@ PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
 
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.sys.usb.udc=700d0000.xudc \
-    sys.usb.controller=700d0000.xudc
+    ro.vendor.nv.usb.pid.adb=7104 \
+    ro.vendor.nv.usb.pid.accessory.adb=7105 \
+    ro.vendor.nv.usb.pid.audio_source.adb=7106 \
+    ro.vendor.nv.usb.pid.ecm=710B \
+    ro.vendor.nv.usb.pid.ecm.adb=710C \
+    ro.vendor.nv.usb.pid.midi=7109 \
+    ro.vendor.nv.usb.pid.midi.adb=710A \
+    ro.vendor.nv.usb.pid.mtp=2000 \
+    ro.vendor.nv.usb.pid.mtp.adb=2000 \
+    ro.vendor.nv.usb.pid.ncm=7107 \
+    ro.vendor.nv.usb.pid.ncm.adb=7108 \
+    ro.vendor.nv.usb.pid.ptp=2000 \
+    ro.vendor.nv.usb.pid.ptp.adb=2000 \
+    ro.vendor.nv.usb.pid.rndis=2000 \
+    ro.vendor.nv.usb.pid.rndis.acm.adb=AF00 \
+    ro.vendor.nv.usb.pid.rndis.adb=2000 \
+    ro.vendor.nv.usb.vid=057E \
+    sys.usb.controller=700d0000.xudc \
+    vendor.sys.usb.udc=700d0000.xudc

23163a70bb4a157575754faa7b5e3bc8fb63b58c
Thomas Makin <halorocker89@gmail.com>
2022-11-27 20:53:21 -0500

    nx: sensors: switch to hidl 2 sensor hal
    Change-Id: I8dff7a4ae92cc608421c44c7b7708d699355bf1e

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_SYSTEM)etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 ifneq ($(PRODUCT_IS_ATV),true)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
@@ -142,15 +142,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # STMicroElectronics IMU
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-service \
-    android.hardware.sensors@1.0-impl \
-    sensors.stmicro \
-    sensors_calib
+    android.hardware.sensors@2.0-service.stmicroelectronics
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml
 
 # Thermal
 PRODUCT_PACKAGES += \
--- properties.mk
+++ properties.mk
@@ -14,6 +14,14 @@ PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.tegra.stb.mode=1
 
+# Sensor HAL
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.stm.sensors.max-odr=2000 \
+    persist.vendor.stm.sensors.rot-matrix-1.accel="0,1,0,-1,0,0,0,0,1" \
+    persist.vendor.stm.sensors.rot-matrix-2.gyro="0,1,0,-1,0,0,0,0,1" \
+    persist.vendor.stm.sensors.max-range.accel=79 \
+    persist.vendor.stm.sensors.max-range.gyro=35
+
 # USB configfs
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.nv.usb.pid.adb=7104 \

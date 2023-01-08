461324294b167e4e3672e690b8f9e9f0f3e03f60
Thomas Makin <halorocker89@gmail.com>
2023-01-07 18:35:00 -0900

    [TO-SPLIT/CLEAN] nx: Build new bootstack and add Mariko support
    - Build bl31 (ATF) and bl33 (U-Boot)
    - Make boot.scr optional (and don't build by default) and use inline script
    - Nuke Coreboot etc. in favor of L4T-Loader
    - Build and use dtimg
    - Use proper kernel load addr in bootimg header
    - Copy wifi nvram for all skus
    - Update releasetools
    - Update hekate ini
    
    Change-Id: I78ca9cb1ec02be73be48377aeeee06d2d0ad4318

--- vendor/bcm_firmware/Android.mk
+++ vendor/bcm_firmware/Android.mk
@@ -13,43 +13,91 @@
 # limitations under the License.
 
 LOCAL_PATH := $(call my-dir)
+COMMON_BCM_PATH := ../../../../../vendor/nvidia/foster/bcm_firmware
 FOSTER_BCM_PATH := ../../../../../vendor/nvidia/foster/bcm_firmware
 NX_BCM_PATH := ../../../../../vendor/nintendo/bcm_firmware
 
+# Bluetooth Patchfile
 include $(CLEAR_VARS)
 LOCAL_MODULE        := BCM4356A3
 LOCAL_SRC_FILES     := $(NX_BCM_PATH)/CYW4356A3_001.004.009.0092.0095.bin
 LOCAL_MODULE_SUFFIX := .hcd
 LOCAL_MODULE_CLASS  := ETC
-LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware/brcm
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_OWNER  := nintendo
 include $(BUILD_PREBUILT)
 
+# Unified target for Switch brcmfmac4356A3 wifi
 include $(CLEAR_VARS)
-LOCAL_MODULE               := brcmfmac4356A3-pcie.bin
-LOCAL_MODULE_CLASS         := ETC
-LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE        := brcmfmac4356A3-pcie
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
 
-_brcmfmac4356A3_input         := $(BUILD_TOP)/vendor/nintendo/bcm_firmware/brcmfmac4356A3-pcie.bin
+# mdarcy fw and clm, plus hos nvram copies with ccode mod
+LOCAL_REQUIRED_MODULES := \
+	brcmfmac4356-pcie \
+	brcmfmac4356-pcie.clm_blob \
+	brcmfmac4356-pcie.nvidia,odin.txt \
+	brcmfmac4356-pcie.nvidia,modin.txt \
+	brcmfmac4356-pcie.nvidia,vali.txt \
+	brcmfmac4356-pcie.nvidia,frig.txt
+
 _brcmfmac4356A3_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
 _brcmfmac4356A3_archive       := $(_brcmfmac4356A3_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 
 $(_brcmfmac4356A3_archive):
 	@mkdir -p $(dir $@)
-	@mkdir -p $(TARGET_OUT_VENDOR)/firmware
-	cp $(_brcmfmac4356A3_input) $(_brcmfmac4356A3_intermediates)
-	cp $(_brcmfmac4356A3_input) $(TARGET_OUT_VENDOR)/firmware/brcmfmac4356-pcie.bin
+	@touch $(_brcmfmac4356A3_archive)
 
-LOCAL_MODULE_TAGS          := optional
-LOCAL_MODULE_OWNER         := nintendo
 include $(BUILD_SYSTEM)/base_rules.mk
 
+# mdarcy clm target
 include $(CLEAR_VARS)
-LOCAL_MODULE               := brcmfmac4356-pcie.nintendo,icosa.txt
-LOCAL_SRC_FILES            := $(NX_BCM_PATH)/brcmfmac4356A3-pcie.txt
-LOCAL_MODULE_CLASS         := ETC
-LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)/firmware
-LOCAL_MODULE_TAGS          := optional
-LOCAL_MODULE_OWNER         := nintendo
+LOCAL_MODULE        := brcmfmac4356-pcie.clm_blob
+LOCAL_SRC_FILES     := $(FOSTER_BCM_PATH)/bcm4356/brcmfmac4356-pcie.clm_blob
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
 include $(BUILD_PREBUILT)
+
+# HOS nvram copies (if we don't dup the target we can't guarantee all files will be there on subsequent builds)
+include $(CLEAR_VARS)
+LOCAL_MODULE        := brcmfmac4356-pcie.nvidia,odin.txt
+LOCAL_SRC_FILES     := $(NX_BCM_PATH)/brcmfmac4356A3-pcie.txt
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := brcmfmac4356-pcie.nvidia,modin.txt
+LOCAL_SRC_FILES     := $(NX_BCM_PATH)/brcmfmac4356A3-pcie.txt
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := brcmfmac4356-pcie.nvidia,vali.txt
+LOCAL_SRC_FILES     := $(NX_BCM_PATH)/brcmfmac4356A3-pcie.txt
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := brcmfmac4356-pcie.nvidia,frig.txt
+LOCAL_SRC_FILES     := $(NX_BCM_PATH)/brcmfmac4356A3-pcie.txt
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/firmware
+LOCAL_MODULE_TAGS   := optional
+LOCAL_MODULE_OWNER  := nintendo
+include $(BUILD_PREBUILT)
+

834c3363e1436aa349bdff6c52688131ab182032
Thomas Makin <halorocker89@gmail.com>
2023-11-12 14:29:02 -0500

    nx: use intel iio/sysfs sensor hal
    Set up in polling mode--trigger and event mode will need testing. Will
    also require testing for generalized calib profile for max range.
    
    Change-Id: I8e58cac0288ba8b53f3451c239c15c0a906db8d4

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -60,6 +60,14 @@ LOCAL_SRC_FILES    := power.nx.rc
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE               := init.sensors.nx.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.sensors.nx.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE        := lkm_loader
 LOCAL_SRC_FILES     := lkm_loader.sh
 LOCAL_MODULE_SUFFIX := .sh

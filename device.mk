eb64a67f176be875d8b994046827b9520c3c89be
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-26 12:34:43 -0500

    nx: Fixup tab usage
    Change-Id: I9792cc16fabbfb58937cef2f3c1a9320ea5e8550

--- device.mk
+++ device.mk
@@ -64,8 +64,8 @@ PRODUCT_SOONG_NAMESPACES += device/nintendo/nx
 # Init related
 PRODUCT_PACKAGES += \
     fstab.nx \
-	init.vali.rc \
-	init.frig.rc \
+    init.vali.rc \
+    init.frig.rc \
     init.loki_foster_e_common.rc \
     init.nx.rc \
     init.recovery.nx.rc \

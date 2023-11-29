e4b65672a6463d437518af7af1875d54e4dcf5ca
Thomas Makin <halorocker89@gmail.com>
2023-11-29 14:58:11 -0500

    devicesettings: refactor and clean up
    - Migrate static prefs to xml
    - Migrate static arrays to resources
    - Migrate to API 30
    - Stop using deprecated APIs
    - Migrate to AppCompat
    - Fix formatting
    
    Change-Id: Ie9b7ddce51ac7c6e0575eb051d76cb98ad46fa2c

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -14,7 +14,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
-    androidx.preference_preference \
+    androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.lineageos.platform.internal \

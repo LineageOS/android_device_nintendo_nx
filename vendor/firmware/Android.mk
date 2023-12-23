2ca4c3b2b4e77fd57b736240e3d50d6c5bb23ee9
Thomas Makin <halorocker89@gmail.com>
2023-12-23 14:14:36 -0500

    nx: Use common atf target
    * Introduced in 19.1
    
    Change-Id: I3f1132c45ea0392ef31b0139753344267c017bf8

--- vendor/firmware/Android.mk
+++ vendor/firmware/Android.mk
@@ -14,42 +14,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ATF_PATH   := $(BUILD_TOP)/hardware/nintendo/arm-trusted-firmware
 UBOOT_PATH := $(BUILD_TOP)/hardware/nintendo/u-boot
 
 BUILD_TOOLS_BINS         := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin
 LINEAGE_TOOLS_PATH       := $(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin
 TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)
-
-# Platform defaults
-BL31_MAKEARGS := PLAT=tegra TARGET_SOC=t210 TZDRAM_BASE=0xFFF00000 RESET_TO_BL31=1 COLD_BOOT_SINGLE_CPU=1 \
-                 PROGRAMMABLE_RESET_ADDRESS=1 ENABLE_STACK_PROTECTOR=none
-
-# Not supported in Linux 4.9
-BL31_MAKEARGS += SDEI_SUPPORT=0
-
-# Error reporting
-BL31_MAKEARGS += CRASH_REPORTING=1 ENABLE_ASSERTIONS=1 LOG_LEVEL=0 PLAT_LOG_LEVEL_ASSERT=0
-
-# ATF
-include $(CLEAR_VARS)
-LOCAL_MODULE        := bl31
-LOCAL_MODULE_SUFFIX := .bin
-LOCAL_MODULE_CLASS  := EXECUTABLES
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
-
-_atf_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_atf_bin := $(_atf_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
-
-$(_atf_bin):
-	@mkdir -p $(dir $@)
-	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_CROSS_COMPILE) \
-	CLANG_CCDIR=$(TARGET_KERNEL_CLANG_PATH)/bin/ CC=$(TARGET_KERNEL_CLANG_PATH)/bin/clang -C $(ATF_PATH) \
-	BUILD_BASE=$(abspath $(_atf_intermediates)) $(BL31_MAKEARGS) bl31
-	@cp $(dir $@)/tegra/t210/release/bl31.bin $@
-
-include $(BUILD_SYSTEM)/base_rules.mk
-INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/bl31.bin
 
 # U-Boot
 include $(CLEAR_VARS)
--- vendor/nx-vendor.mk
+++ vendor/nx-vendor.mk
@@ -18,6 +18,18 @@ include device/nintendo/nx/vendor/bcm_firmware/bcm.mk
 
 PRODUCT_PACKAGES += public.libraries
 
+ATF_PATH   := $(BUILD_TOP)/hardware/nintendo/arm-trusted-firmware
+
+# Platform defaults
+ATF_PARAMS := PLAT=tegra TARGET_SOC=t210 TZDRAM_BASE=0xFFF00000 RESET_TO_BL31=1 COLD_BOOT_SINGLE_CPU=1 \
+                 PROGRAMMABLE_RESET_ADDRESS=1 ENABLE_STACK_PROTECTOR=none
+
+# Not supported in Linux 4.9
+ATF_PARAMS += SDEI_SUPPORT=0
+
+# Error reporting
+ATF_PARAMS += CRASH_REPORTING=1 ENABLE_ASSERTIONS=1 LOG_LEVEL=0 PLAT_LOG_LEVEL_ASSERT=0
+
 # Switch firmware files
 PRODUCT_PACKAGES += \
 	android.ini \
@@ -25,3 +37,5 @@ PRODUCT_PACKAGES += \
 	icon_android_hue \
 	bl31 \
 	bl33
+
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/bl31.bin

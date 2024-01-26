LineageOS/android_device_nvidia_galen

Remove twrp build support
Change-Id: Ifeb70c254ac21aa60311fec50d0194bdcef80e57

AndroidProducts.mk
@@ -16,8 +16,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_galen.mk \
-    $(LOCAL_DIR)/lineage_galen.mk \
-    $(LOCAL_DIR)/twrp_galen.mk
+    $(LOCAL_DIR)/lineage_galen.mk
 
 COMMON_LUNCH_CHOICES := \
     full_galen-eng \
@@ -25,6 +24,4 @@ COMMON_LUNCH_CHOICES := \
     full_galen-userdebug \
     lineage_galen-eng \
     lineage_galen-user \
-    lineage_galen-userdebug \
-    twrp_galen-eng \
-    twrp_galen-userdebug
+    lineage_galen-userdebug

LineageOS/android_device_nintendo_nx

AndroidProducts.mk
@@ -15,12 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nx.mk \
-    $(LOCAL_DIR)/twrp_nx.mk
+    $(LOCAL_DIR)/lineage_nx.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_nx-eng \
     lineage_nx-user \
-    lineage_nx-userdebug \
-    twrp_nx-eng \
-    twrp_nx-userdebug
+    lineage_nx-userdebug
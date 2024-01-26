LineageOS/android_device_nvidia_galen

Convert vendor makefile inherits to includes
Some variables that need checked in the vendor makefiles are being
purged by the inherit macro.

Change-Id: I3942e953ff0fb3adfaabc7189a92bff92818de0d

device.mk
@@ -41,7 +41,7 @@ AB_OTA_UPDATER := true
 include device/nvidia/t194-common/t194.mk
 
 # System properties
-include $(LOCAL_PATH)/system_prop.mk
+include device/nvidia/galen/system_prop.mk
 
 PRODUCT_CHARACTERISTICS  := tv
 PRODUCT_AAPT_CONFIG      := xlarge large
@@ -51,7 +51,7 @@ TARGET_SCREEN_WIDTH      := 1080
 
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
-$(call inherit-product, device/nvidia/galen/vendor/galen-vendor.mk)
+include device/nvidia/galen/vendor/galen-vendor.mk
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/nvidia/galen

LineageOS/android_device_nintendo_nx

device.mk
@@ -33,7 +33,7 @@ TARGET_TEGRA_WIREGUARD ?= compat
 include device/nvidia/t210-common/t210.mk
 
 # Properties
-include $(LOCAL_PATH)/properties.mk
+include device/nintendo/nx/properties.mk
 
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi tvdpi mdpi
 PRODUCT_AAPT_CONFIG := normal large xlarge
@@ -47,7 +47,7 @@ endif
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 
-$(call inherit-product, device/nintendo/nx/vendor/nx-vendor.mk)
+include device/nintendo/nx/vendor/nx-vendor.mk
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
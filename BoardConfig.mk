LineageOS/android_device_nvidia_foster

Move bluetooth name to bluetooth.prop
* Sadly in PRODUCT_PROPERTY_OVERRIDES this
  translates to 3 seperate props, example:

```
bluetooth.device.default_name=SHIELD
Android
TV
```

* The build system flips out when quotes are
  used.

* Can't move whole device to *.prop because we
  ifdef so many of these in tegra trees that it
  would be disgusting.

Change-Id: I4c611c64652d28f65505382db7b3e08bc6f3fabf

BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_BOARD_INFO_FILE := device/nvidia/foster/board-info.txt
 DEVICE_MANIFEST_FILE := device/nvidia/foster/manifest.xml
 
 # Bluetooth
+TARGET_VENDOR_PROP += device/nvidia/foster/bluetooth.prop
 ifeq ($(TARGET_TEGRA_BT),bcm)
 BOARD_CUSTOM_BT_CONFIG := device/nvidia/foster/comms/vnd_foster.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nvidia/foster/comms

LineageOS/android_device_nintendo_nx

BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_BOARD_INFO_FILE := device/nintendo/nx/board-info.txt
 DEVICE_MANIFEST_FILE := device/nintendo/nx/manifest.xml
 
 # Bluetooth
+TARGET_VENDOR_PROP += device/nintendo/nx/bluetooth.prop
 ifeq ($(TARGET_TEGRA_BT),bcm)
 BOARD_CUSTOM_BT_CONFIG := device/nintendo/nx/comms/vnd_nx.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/nintendo/nx/comms
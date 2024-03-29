326a1a06a0ef87c599266a60cceb96d09dff5754
Nolen Johnson <johnsonnolen@gmail.com>
2024-03-28 23:23:42 -0400

    Revert "fixup! Build nx dtimage"
    This reverts commit fbae64954c265e5b41ca14ca6b5ee57f7d2fb5b6.
    
    Reason: 0x00 is invalid, update u-boot instead.
    Change-Id: Ida25669d36d52c6df6236cfcfbf9ea2a18c7cfc1

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -21,10 +21,10 @@ $(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) | mkdtimg
 	echo -e ${CL_GRN}"Building nx DTImage"${CL_RST}
 	@mkdir -p $(PRODUCT_OUT)/install
 	$(HOST_OUT_EXECUTABLES)/mkdtimg create $@ --page_size=0x1000 \
-		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210-odin.dtb    --id=0x4F44494E --rev=0x00 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210-odin.dtb    --id=0x4F44494E --rev=0xa00 \
 		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-odin.dtb --id=0x4F44494E --rev=0xb01 \
-		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-vali.dtb --id=0x56414C49 --rev=0x00 \
-		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-frig.dtb --id=0x46524947 --rev=0x00
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-vali.dtb --id=0x56414C49 --rev=0xa00 \
+		$(KERNEL_OUT)/arch/arm64/boot/dts/tegra210b01-frig.dtb --id=0x46524947 --rev=0xa00
 
 ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_DTBIMAGE_TARGET)
 

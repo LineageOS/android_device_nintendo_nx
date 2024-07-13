d535e8dde55b7b473a26d20ccb8fa42c027ac2dc
Nolen Johnson <johnsonnolen@gmail.com>
2024-07-12 20:24:55 -0400

    nx: initfiles: lmk_loader_target: Change cfg module name
    * For newer base.
    
    Change-Id: I68c70b5125c54320e3db579c5353f8fd2154f49a

--- initfiles/lkm_loader_target.sh
+++ initfiles/lkm_loader_target.sh
@@ -3,6 +3,6 @@
 /vendor/bin/insmod /vendor/lib/modules/pci-tegra.ko
 
 /vendor/bin/insmod /vendor/lib/modules/compat.ko
-/vendor/bin/insmod /vendor/lib/modules/cfg80211.ko
+/vendor/bin/insmod /vendor/lib/modules/cy_cfg80211.ko
 /vendor/bin/insmod /vendor/lib/modules/brcmutil.ko
 /vendor/bin/insmod /vendor/lib/modules/brcmfmac.ko

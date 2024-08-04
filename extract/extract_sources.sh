8e1c4941e95229648d5916a55f0299498a172503
Nolen Johnson <johnsonnolen@gmail.com>
2024-08-03 20:48:16 -0400

    nx: extract: Redo
    Change-Id: Ib4b4147c5963de625555036c6fd9998279f889bf

--- extract/extract_sources.sh
+++ extract/extract_sources.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,5 @@
 # limitations under the License.
 #
 
-FILELIST_PATHS+=("nx");
+FILELIST_PATHS+=("nintendo/nx");
+PATCH_PATHS+=("nintendo/nx");

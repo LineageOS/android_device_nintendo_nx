ba7671171a7c9aa6a720a435ae810b3217347706
Thomas Makin <halorocker89@gmail.com>
2024-12-21 15:00:10 +0000

    [WIP] nx: migrate to powerhint
    Exercise greater control over hinting etc.
    Better support in the future vs maintaining phs
    Totally nuke cpl
    
    Change-Id: If9076248fcacfdc8165bd814ad78786134997b97

--- device.mk
+++ device.mk
@@ -18,11 +18,13 @@ TARGET_TEGRA_VARIANT    ?= common
 
 TARGET_TEGRA_BT       ?= bcm
 TARGET_TEGRA_CEC      := aosp
+TARGET_TEGRA_CPL      := none
 TARGET_TEGRA_KERNEL   ?= 4.9
 TARGET_TEGRA_KEYSTORE := software
 TARGET_TEGRA_LIGHT    ?= lineage
 TARGET_TEGRA_MAN_LVL  := 5
 TARGET_TEGRA_MEMTRACK ?= rel-shield-r
+TARGET_TEGRA_POWER    := perfmgr
 TARGET_TEGRA_THERMAL  ?= lineage
 TARGET_TEGRA_UBOOT    := prebuilt
 TARGET_TEGRA_WIDEVINE ?= rel-shield-r
@@ -164,8 +166,16 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 # PHS
+ifeq ($(TARGET_TEGRA_POWER),perfmgr)
+PRODUCT_PACKAGES += \
+	powerhint.odin.json \
+	powerhint.modin.json \
+	powerhint.vali.json \
+	powerhint.fric.json
+else
 PRODUCT_PACKAGES += \
     nvphsd.conf
+endif
 
 # Recovery
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.vendor.recovery_update=true

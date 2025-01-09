2a88ff5bdc4bc90f5eb5bed17a88dc96c4153ef3
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-08 23:21:39 -0500

    vendor: Use copy-files for firmware targets
    Change-Id: Ic8fd23f385f74d2c35cfa83e4823ba99d279a4ee

--- build/tasks/bootloader.mk
+++ build/tasks/bootloader.mk
@@ -12,44 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
+ifneq ($(filter nx nx_tab,$(TARGET_DEVICE)),)
 
 UBOOT_PATH := $(BUILD_TOP)/hardware/nintendo/u-boot
-NX_FW_PATH := vendor/nintendo/nx/rel-shield-r/
+NX_FW_PATH := vendor/nintendo/nx/rel-shield-r
 
 BUILD_TOOLS_BINS         := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin
 LINEAGE_TOOLS_PATH       := $(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin
 TARGET_KERNEL_CLANG_PATH ?= $(BUILD_TOP)/prebuilts/clang/host/$(HOST_PREBUILT_TAG)/$(LLVM_PREBUILTS_VERSION)
 
-# U-Boot
-include $(CLEAR_VARS)
-LOCAL_MODULE        := bl33
-LOCAL_MODULE_SUFFIX := .bin
-LOCAL_MODULE_CLASS  := EXECUTABLES
-LOCAL_MODULE_PATH   := $(PRODUCT_OUT)
+ifneq ($(TARGET_TEGRA_UBOOT),prebuilt)
 
-ifeq ($(TARGET_TEGRA_UBOOT),prebuilt)
-LOCAL_SRC_FILES        := ../../../../../$(NX_FW_PATH)/bl33.bin
-LOCAL_REQUITED_MODULES := boot.scr
-include $(BUILD_PREBUILT)
-else # TARGET_TEGRA_UBOOT
-_uboot_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
-_uboot_bin := $(_uboot_intermediates)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+_uboot_bin := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
+_uboot_bin := $(_uboot_bin)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 
 $(_uboot_bin): $(sort $(shell find -L $(UBOOT_PATH)))
 	@mkdir -p $(dir $@)
 	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_CROSS_COMPILE) \
 		HOSTCC=$(TARGET_KERNEL_CLANG_PATH)/bin/clang HOSTLDFLAGS="-fuse-ld=lld" \
 		YACC=$(BUILD_TOOLS_BINS)/bison LEX=$(BUILD_TOOLS_BINS)/flex M4=$(BUILD_TOOLS_BINS)/m4 \
-		-C $(UBOOT_PATH) O=$(abspath $(_uboot_intermediates)) nx_defconfig
+		-C $(UBOOT_PATH) O=$(dir $(_uboot_bin)) nx_defconfig
 	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_CROSS_COMPILE) \
 		HOSTCC=$(TARGET_KERNEL_CLANG_PATH)/bin/clang HOSTLDFLAGS="-fuse-ld=lld" \
 		YACC=$(BUILD_TOOLS_BINS)/bison LEX=$(BUILD_TOOLS_BINS)/flex M4=$(BUILD_TOOLS_BINS)/m4 \
-		-C $(UBOOT_PATH) O=$(abspath $(_uboot_intermediates)) u-boot-dtb.bin
-	@mv $(_uboot_intermediates)/u-boot-dtb.bin $(_uboot_bin)
+		-C $(UBOOT_PATH) O=$(dir $(_uboot_bin)) bl33.bin
+	@mv $(_uboot_bin)/bl33.bin $(_uboot_bin)
 
-include $(BUILD_SYSTEM)/base_rules.mk
+$(PRODUCT_OUT)/bl33.bin: $(_uboot_bin)
+	$(hide) cp $< $@
+.PHONY: bl33
+u-boot-dtb: $(PRODUCT_OUT)/bl33.bin
 endif # TARGET_TEGRA_UBOOT
 
-INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/bl33.bin
-INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/bl31.bin
+_uscript_input := $(abspath device/nintendo/nx/bootfiles/android_boot.txt)
+_uscript_archive := $(call intermediates-dir-for,EXECUTABLES,boot.scr)
+$(_uscript_archive): $(_uscript_input)
+        @mkdir -p $(dir $@) \
+        $(LINEAGE_TOOLS_PATH)/mkimage -A arm -T script -O linux -d $(_uscript_input) $(_uscript_archive)/boot.scr
+
+$(PRODUCT_OUT)/boot.scr: $(_uscript_archive)
+        $(hide) cp $< $@
+.PHONY: boot.scr
+boot.scr: $(PRODUCT_OUT)/boot.scr
+
+endif # TARGET_DEVICE

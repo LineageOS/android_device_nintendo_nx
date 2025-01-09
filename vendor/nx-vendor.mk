d06e04e1c34946a90571654884e2f5aa629f6519
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-09 12:36:56 -0500

    nx: Copy NVRAM per SKU
    Change-Id: Ic65d55d1d85d479da63ec933473b39fc85c1c7ee

--- vendor/nx-vendor.mk
+++ vendor/nx-vendor.mk
@@ -33,7 +33,10 @@ NX_BCM_SWR_PATH := vendor/nintendo/nx/swr/bcm
 
 PRODUCT_COPY_FILES += \
     $(NX_BCM_PATH)/bcm4356/brcmfmac4356-pcie.clm_blob:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.clm_blob \
-    $(NX_BCM_SWR_PATH)/bcm4356/brcmfmac4356A3-pcie.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.txt \
+    $(NX_BCM_SWR_PATH)/bcm4356/brcmfmac4356A3-pcie.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.nvidia,odin.txt \
+    $(NX_BCM_SWR_PATH)/bcm4356/brcmfmac4356A3-pcie.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.nvidia,modin.txt \
+    $(NX_BCM_SWR_PATH)/bcm4356/brcmfmac4356A3-pcie.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.nvidia,vali.txt \
+    $(NX_BCM_SWR_PATH)/bcm4356/brcmfmac4356A3-pcie.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/brcmfmac4356-pcie.nvidia,fric.txt \
     $(NX_BCM_SWR_PATH)/bcm4356/CYW4356A3_001.004.009.0092.0095.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM4356A3.hcd
 
 PRODUCT_COPY_FILES += \

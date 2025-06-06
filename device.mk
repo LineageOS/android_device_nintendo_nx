f638e3eef3f979925f8e608ccda284e87e6768ff
Thomas Makin <halorocker89@gmail.com>
2025-06-06 14:51:43 +0000

    Revert "nx: Update fingerprint to 9.2.0"
    Reason for revert: Breaks GFN.
    
    Change-Id: Ie457f80c10bc52dd6fb4ced38bf133cc308a3fcb

--- device.mk
+++ device.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=NVIDIA/foster_e/foster:11/RQ1A.210105.003/13961456_3871.0251:user/release-keys
+    BuildFingerprint=NVIDIA/nx/nx:11/RQ1A.210105.003/13961456_3871.0251:user/release-keys
 
 # GMS
 ifeq ($(WITH_GMS),true)

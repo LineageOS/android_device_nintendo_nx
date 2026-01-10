40f8d9ab25c42cdcc65f25385d17caa6dfe58365
Thomas Makin <halorocker89@gmail.com>
2026-01-10 23:28:48 +0000

    devicesettings: update androidx components
    - SwitchPreference -> SwitchPreferenceCompat
    - android.app.AlertDialog -> androidx.appcompat.app.AlertDialog;
    
    Change-Id: Ic2e04f0b5900fb0a23b5630c13eaca0b0e03effe

--- devicesettings/src/org/lineageos/settings/device/DisplaySettingsFragment.java
+++ devicesettings/src/org/lineageos/settings/device/DisplaySettingsFragment.java
@@ -16,7 +16,6 @@
 
 package org.lineageos.settings.device;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -25,6 +24,8 @@ import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.util.Log;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;

dd19f6b68871a10e48f785a3e85ca5b5141f459a
Nolen Johnson <johnsonnolen@gmail.com>
2026-01-10 22:34:13 -0500

    nx: Squashed restoration of RSMouse notifications
    This is a squashed revert restoring the following:
    ```
    commit 691e69edf4a060b9228954b4c9674a9b013bef4e
    Author: Thomas Makin <halorocker89@gmail.com>
    Date:   Sun Jan 11 01:46:28 2026 +0000
    
        Revert "devicesettings: add rsmouse feature notif"
    
        Revert submission 419502
    
        Reason for revert: need sepolicy
        Reverted changes: /q/submissionid:419502
    
        Change-Id: Iaa64bce011731cf685480078d82e8933dbe4c28a
    
    commit cfb7bf60b35eda85c22148b66daa739488a06feb
    Author: Thomas Makin <halorocker89@gmail.com>
    Date:   Sun Jan 11 01:46:28 2026 +0000
    
        Revert "devicesettings: update androidx components"
    
        Revert submission 419502
    
        Reason for revert: need sepolicy
        Reverted changes: /q/submissionid:419502
    
        Change-Id: I8d9c7fb496bc3e9c30e65cd3cb586f6c3327d6ae
    
    commit e59af96db10855d0414cbb60c799caa192cf0887
    Author: Thomas Makin <halorocker89@gmail.com>
    Date:   Sun Jan 11 01:46:28 2026 +0000
    
        Revert "devicesettings: add rsmouse settings"
    
        Revert submission 419502
    
        Reason for revert: need sepolicy
        Reverted changes: /q/submissionid:419502
    
        Change-Id: I3325fa5b6124dcd3c0bb0b9bf2cc8ecc3a00e3f4
    ```
    
    Change-Id: I7d1e56a41b999fb09db5b0c48d46ad150eaa3cfd

--- devicesettings/src/org/lineageos/settings/device/DisplaySettingsFragmentLeanback.java
+++ devicesettings/src/org/lineageos/settings/device/DisplaySettingsFragmentLeanback.java
@@ -18,7 +18,6 @@ package org.lineageos.settings.device;
 
 import java.util.HashMap;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -28,6 +27,7 @@ import android.os.SystemProperties;
 import android.util.Log;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.leanback.preference.LeanbackPreferenceFragmentCompat;
 import androidx.leanback.preference.LeanbackSettingsFragmentCompat;
 import androidx.preference.DialogPreference;
@@ -121,6 +121,8 @@ public class DisplaySettingsFragmentLeanback extends LeanbackSettingsFragmentCom
             if (!sku.equals("vali")) {
                 commonPrefs.createDisplaySettings(preferenceScreen);
             }
+
+            commonPrefs.createJoyConSettings(preferenceScreen);
         }
 
         @Override
